9e82cf05661e46250b98f14562a8adb3b5ca6b5b
Stavarengo <r.stavarengo@youweagency.com>
2021-01-06 16:19:37 +0100

    feat(default-mediator): Create \Dominoes\GameMediator\DefaultMediator\State\NotStarted

---
+++ src/Dominoes/GameMediator/DefaultMediator/State/NotStarted.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Dominoes\GameMediator\DefaultMediator\State;
+
+
+use Dominoes\GameMediator\Exception\GameDidNotStartYet;
+use Dominoes\GameMediator\GameMediatorInterface;
+use Dominoes\LineOfPlay\ConnectionSpot\ConnectionSpotInterface;
+use Dominoes\Player\PlayerInterface;
+use Dominoes\Tile\TileInterface;
+
+class NotStarted extends AbstractState
+{
+    public function getConnectionSpots(): array
+    {
+        throw GameDidNotStartYet::create();
+    }
+
+    public function getCurrentPlayerTiles(): array
+    {
+        throw GameDidNotStartYet::create();
+    }
+
+    public function connectTile(TileInterface $tile, ConnectionSpotInterface $connectionSpot,): void
+    {
+        throw GameDidNotStartYet::create();
+    }
+
+    public function start(PlayerInterface ...$players): void
+    {
+        $this->gameMediator->getRoundManager()->setPlayers(...$players);
+
+        $this->gameMediator->changeState(new InProgress($this->gameMediator));
+    }
+
+    public function drawOrPass(): void
+    {
+        throw GameDidNotStartYet::create();
+    }
+
+    public function getStatus(): int
+    {
+        return GameMediatorInterface::STATUS_NOT_STARTED;
+    }
+
+    public function getWinner(): ?PlayerInterface
+    {
+        return null;
+    }
+}
---
+++ test/Dominoes/GameMediator/DefaultMediator/State/NotStartedTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Dominoes\GameMediator\DefaultMediator\State;
+
+use Dominoes\GameMediator\DefaultMediator\DefaultMediator;
+use Dominoes\GameMediator\DefaultMediator\State\InProgress;
+use Dominoes\GameMediator\DefaultMediator\State\NotStarted;
+use Dominoes\GameMediator\Exception\GameDidNotStartYet;
+use Dominoes\GameMediator\GameMediatorInterface;
+use Dominoes\LineOfPlay\ConnectionSpot\ConnectionSpotInterface;
+use Dominoes\Player\PlayerInterface;
+use Dominoes\RoundManager\RoundManagerInterface;
+use Dominoes\Tile\TileInterface;
+use PHPUnit\Framework\TestCase;
+
+class NotStartedTest extends TestCase
+{
+    public function testGetConnectionSpots()
+    {
+        $this->expectExceptionObject(GameDidNotStartYet::create());
+        $state = new NotStarted($this->createStub(DefaultMediator::class));
+        $state->getConnectionSpots();
+    }
+
+    public function testGetCurrentPlayerTiles()
+    {
+        $this->expectExceptionObject(GameDidNotStartYet::create());
+        $state = new NotStarted($this->createStub(DefaultMediator::class));
+        $state->getCurrentPlayerTiles();
+    }
+
+    public function testConnectTile()
+    {
+        $this->expectExceptionObject(GameDidNotStartYet::create());
+        $state = new NotStarted($this->createStub(DefaultMediator::class));
+        $state->connectTile($this->createStub(TileInterface::class), $this->createStub(ConnectionSpotInterface::class));
+    }
+
+    public function testStart()
+    {
+        $player = $this->createStub(PlayerInterface::class);
+
+        $roundManager = $this->createMock(RoundManagerInterface::class);
+        $roundManager->expects($this->once())
+            ->method('setPlayers')
+            ->with($this->identicalTo($player));
+
+        $gameMediator = $this->createMock(DefaultMediator::class);
+        $gameMediator->expects($this->once())
+            ->method('changeState')
+            ->with($this->isInstanceOf(InProgress::class));
+        $gameMediator->expects($this->once())
+            ->method('getRoundManager')
+            ->willReturn($roundManager);
+
+        $state = new NotStarted($gameMediator);
+        $state->start($player);
+    }
+
+    public function testDrawOrPass()
+    {
+        $this->expectExceptionObject(GameDidNotStartYet::create());
+        $state = new NotStarted($this->createStub(DefaultMediator::class));
+        $state->drawOrPass();
+    }
+
+    public function testGetStatus()
+    {
+        $state = new NotStarted($this->createStub(DefaultMediator::class));
+        $this->assertEquals(GameMediatorInterface::STATUS_NOT_STARTED, $state->getStatus());
+    }
+
+    public function testGetWinner()
+    {
+        $state = new NotStarted($this->createStub(DefaultMediator::class));
+        $this->assertNull($state->getWinner());
+    }
+}
