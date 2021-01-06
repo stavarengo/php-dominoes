70493753e443a492ac93ed6148890264ecd5feca
Stavarengo <r.stavarengo@youweagency.com>
2021-01-06 15:36:51 +0100

    feat(game-mediator): Create \Dominoes\GameMediator\GameMediatorInterface

---
+++ src/Dominoes/GameMediator/Exception/GameException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Dominoes\GameMediator\Exception;
+
+
+class GameException extends \RuntimeException
+{
+
+}
---
+++ src/Dominoes/GameMediator/GameMediatorInterface.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Dominoes\GameMediator;
+
+
+use Dominoes\GameMediator\Exception\GameException;
+use Dominoes\LineOfPlay\ConnectionSpot\ConnectionSpotInterface;
+use Dominoes\Player\PlayerInterface;
+use Dominoes\Tile\TileInterface;
+
+interface GameMediatorInterface
+{
+    public const STATUS_NOT_STARTED = 1;
+    public const STATUS_IN_PROGRESS = 2;
+    public const STATUS_GAME_IS_OVER = 3;
+
+    /**
+     * @return ConnectionSpotInterface[]
+     *
+     * @throws GameException
+     */
+    public function getConnectionSpots(): array;
+
+    /**
+     * @return TileInterface[]
+     * @throws GameException
+     */
+    public function getCurrentPlayerTiles(): array;
+
+    /**
+     * @param TileInterface $tile
+     * @param ConnectionSpotInterface $connectionSpot
+     * @throws GameException
+     */
+    public function connectTile(TileInterface $tile, ConnectionSpotInterface $connectionSpot,): void;
+
+    /**
+     * @param PlayerInterface ...$players
+     * @throws GameException
+     */
+    public function start(PlayerInterface ...$players): void;
+
+    /**
+     * @throws GameException
+     */
+    public function drawOrPass(): void;
+
+    /**
+     * Return the value of one of the constants `STATUS_*`
+     *
+     * @return int
+     * @throws GameException
+     */
+    public function getStatus(): int;
+
+    /**
+     * @return PlayerInterface|null
+     * @throws GameException
+     */
+    public function getWinner(): ?PlayerInterface;
+}
